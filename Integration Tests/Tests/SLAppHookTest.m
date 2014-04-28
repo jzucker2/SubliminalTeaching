e26893e95d90442e462c693e22952db877066245
Jordan Zucker <jzucker@twitter.com>
2014-04-28 04:13:59 -0700

    Added app hook tests

---
+++ Integration Tests/Tests/SLAppHookTest.m
@@ -0,0 +1,37 @@
+//
+//  SLAppHookTest.m
+//  Subliminal
+//
+//  Created by Jordan Zucker on 4/28/14.
+//  Copyright (c) 2014 Inkling. All rights reserved.
+//
+
+#import "SLIntegrationTest.h"
+
+@interface focus_SLAppHookTest : SLIntegrationTest
+
+@end
+
+@implementation focus_SLAppHookTest
+
++ (NSString *)testCaseViewControllerClassName {
+    return @"SLAppHookTestViewController";
+}
+
+// If you override set-up methods,
+// you must call super at the beginning of your implementations.
+
+// If you override tear-down methods,
+// you must call super at the *end* of your implementations.
+
+- (void)testUseAppHookToSetButtonLabel {
+    SLElement *testLabelFoo = [SLElement elementWithAccessibilityLabel:@"foo"];
+    // put code here
+
+    // now test should pass
+    SLElement *testLabelBar = [SLElement elementWithAccessibilityLabel:@"bar"];
+    SLAssertFalse([testLabelFoo isValidAndVisible], @"foo stayed the same");
+    SLAssertTrue([[testLabelBar label] isEqualToString:@"bar"], @"label text did not change");
+}
+
+@end
---
+++ Integration Tests/Tests/SLAppHookTestViewController.m
@@ -0,0 +1,61 @@
+//
+//  SLAppHookTestViewController.m
+//  Subliminal
+//
+//  Created by Jordan Zucker on 4/28/14.
+//  Copyright (c) 2014 Inkling. All rights reserved.
+//
+
+#import "SLTestCaseViewController.h"
+
+#import <Subliminal/SLTestController+AppHooks.h>
+
+@interface SLAppHookTestViewController : SLTestCaseViewController
+
+@end
+
+@interface SLAppHookTestViewController ()
+@property (nonatomic, weak) IBOutlet UIView *testView;
+@property (nonatomic, weak) IBOutlet UIButton *testButton;
+@property (nonatomic, weak) IBOutlet UILabel *testLabel;
+@end
+
+@implementation SLAppHookTestViewController
+
++ (NSString *)nibNameForTestCase:(SEL)testCase {
+    return @"SLAppHookTestViewController";
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view from its nib.
+    // Test case specific configuration is best done using app hooks
+    // triggered from -[SLAppHookTest setUpTestCaseWithSelector:].
+    _testLabel.text = @"foo";
+    _testLabel.isAccessibilityElement = YES;
+    
+}
+
+- (instancetype)initWithTestCaseWithSelector:(SEL)testCase {
+    self = [super initWithTestCaseWithSelector:testCase];
+    if (self) {
+        // Register for app hooks, e.g.
+        [[SLTestController sharedTestController] registerTarget:self forAction:@selector(setTextLabelText:)];
+    }
+    return self;
+}
+
+// Deregister for app hooks, if any
+- (void)dealloc {
+    [[SLTestController sharedTestController] deregisterTarget:self];
+}
+
+#pragma mark - App hooks
+// Put any app hooks below here
+- (void)setTextLabelText:(NSString *)text
+{
+    _testLabel.text = text;
+}
+
+@end
